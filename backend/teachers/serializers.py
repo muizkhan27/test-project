bc5c5503d8f671d8a52d931ddfac095db3ff34e2
usmanasif4 <uasif.se@gmail.com>
2021-10-18 17:36:14 +0500

    add endpoint to fetch month-wise teachers info

--- backend/teachers/serializers.py
+++ backend/teachers/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
+
 from .models import Teacher
+
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
-        fields = '__all__'
+        fields = "__all__"
+
+
+class TeacherMonthInfoSerializer(serializers.Serializer):
+    """
+    Serializer for month-wise teachers info
+    """
+    teacher__name = serializers.CharField(max_length=255)
+    start__month = serializers.IntegerField()
+    duration__sum = serializers.CharField(max_length=255)
